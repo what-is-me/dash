e147bcbee7a295d1c254368d3540602aa145340e
orangeji <orangeji@tencent.com>
2023-07-27 11:21:21 +0800

    fix customer setting

--- src/dash.cpp
+++ src/dash.cpp
@@ -10,9 +10,9 @@ extern "C" {
 
 struct Dash;
 
-void init(void);
+void init(const char* key);
 
-void* dash_create(const char* key);
+void* dash_create(void);
 int dash_insert(void* dash, uint64_t key, uint64_t value);
 int dash_update(void* dash, uint64_t key, uint64_t value);
 uint64_t dash_remove(void* dash, uint64_t key);
